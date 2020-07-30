0fb704dbd7178c3e7d96512c7fde22e384912bd0
StudyTae <dea972@naver.com>
2020-07-30 11:04:51 +0900

    singquote 사용 프리티어

--- config/restApi.js
+++ config/restApi.js
@@ -1,15 +1,15 @@
-import axios from "axios";
+import axios from 'axios';
 
-export const AUTH_KEY = "OyPVN.pAgCG04mkC1YvARe95CDK4OQPQsXU0lxNYcwKdIuOXuZRLK";
+export const AUTH_KEY = 'OyPVN.pAgCG04mkC1YvARe95CDK4OQPQsXU0lxNYcwKdIuOXuZRLK';
 
-export const STORE_KEY = "71304665387239306661787956754738"; // 암호화 복호화 키
+export const STORE_KEY = '71304665387239306661787956754738'; // 암호화 복호화 키
 
-export const MID = "ZP2007000083";
+export const MID = 'ZP2007000083';
 
 export const zeropayApi = axios.create({
-  baseURL: "https://zpg.dev-zeropaypoint.or.kr/",
+  baseURL: 'https://zpg.dev-zeropaypoint.or.kr/',
   headers: {
-    "Content-Type": "application/json; charset=UTF-8",
+    'Content-Type': 'application/json; charset=UTF-8',
     Authorization: `OnlineAK ${AUTH_KEY}`,
   },
 });
